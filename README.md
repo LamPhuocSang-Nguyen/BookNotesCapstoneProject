df6f842bb18cd1fd9f4445b0ad43509293546f56
LamPhuocSang-Nguyen <75714558+LamPhuocSang-Nguyen@users.noreply.github.com>
2024-05-29 09:46:37 +0700

    version4

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ nodemon index.js
 
 # Entity tables database
 ![Reference image](public/screenshort/EntityTable.PNG)
+
+# APT Interaction
+* When submitting a new book entry, cover images are fetched from the [Open Library Covers API](https://openlibrary.org/dev/docs/api/covers) using Axios. A `getBase64Image` function fetches the image and save them to DBMS.
+
+# Access the website in your browser
+`http://localhost:3000`
+
+# Some pictures from my page 
+1. Header of the page
+![Reference image](public/screenshort/home.PNG)
+2. Book entries with `View Notes`, `Delete Book`, `Update Review` features
+![Reference image](public/screenshort/content.PNG)
+3. Adding a new book with the information of the book
+![Reference image](public/screenshort/newbook.PNG)
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import bodyParser from "body-parser";
+import { compile } from "ejs";
 import express from "express";
 import pg from "pg";
 
@@ -7,23 +8,88 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "booknotes",
+    password: "admin",
+    port: 5432,
+  });
+  db.connect();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get("/", async (req, res) => {
-    const ISBN = "0385472579";
+let books = [];
+
+async function getAllBooks()
+{
+    books = [];
+    const result = await db.query("SELECT * FROM books");
+    result.rows.forEach((book) => {
+        books.push(book);
+    });
+}
+
+async function getBase64Image(ISBN)
+{
     const size = "M.jpg";
+    const response = await axios.get(`https://covers.openlibrary.org/b/isbn/${ISBN}-${size}`, { responseType: 'arraybuffer' });
+    const base64Image = Buffer.from(response.data, 'binary').toString('base64');
+    return `data:image/jpeg;base64,${base64Image}`;
+}
+
+//get root
+app.get("/", async (req, res) => {
     try {
-        const response = await axios.get(`https://covers.openlibrary.org/b/isbn/${ISBN}-${size}`, { responseType: 'arraybuffer' });
-        const base64Image = Buffer.from(response.data, 'binary').toString('base64');
-        const imgSrc = `data:image/jpeg;base64,${base64Image}`;
-        res.render("index.ejs", { data: imgSrc });
-        //console.log(imgSrc);
+        await getAllBooks();
+        res.render("index.ejs", { listBooks: books });
     } catch (err) {
         console.log(err);
     }
+});
 
+//new-entry
+app.get("/new-entry", (req, res) => {
+    res.render("newbook.ejs");
+});
+
+//add a book
+app.post("/addbook", async (req, res) =>{
+    const add = req.body.add;
+    const isbn = req.body.isbn;
+    const title = req.body.title;
+    const author = req.body.author;
+    const description = req.body.description;
+    const review = req.body.review;
+    const rating = req.body.rating;
+    const dateread = new Date();
+
+    if(add === "Cancel")
+    {
+        res.redirect("/");
+    }
+    else
+    {
+        try{
+            const imageSrc =  await getBase64Image(isbn);           
+            const result = await db.query("INSERT INTO books(isbn, title, author, description, review, rating, date_read, base64image) VALUES($1,$2,$3,$4,$5,$6,$7,$8)",
+             [isbn,title,author,description,review,rating,dateread,imageSrc]);
+
+            if(result.rowCount > 0)
+            {
+                console.log("Adding is successfully");
+            }
+            else
+            {
+                console.log("Fail");
+            }
+            res.redirect("/");
+        }catch(err){
+            console.log(err);
+        }
+    }
 });
 
  
